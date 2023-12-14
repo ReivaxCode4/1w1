17f8a6c77aca3c82b1d6ba7dbc7a28fcf82cea7d
Xavier Rémy <e6249068@cmaisonneuve.qc.ca>
2023-12-13 22:20:29 -0500

    Amélioration entête, formulaire et footer

---
+++ js/fonctionsStars.js
@@ -0,0 +1,18 @@
+/*
+let stars = document.querySelectorAll(".stars span");
+
+let products = document.querySelectorAll(".stars");
+
+let ratings = [];
+
+for(let star of stars) {
+
+    star.addEventListener("click", function(){
+
+        this.setAttribute("data-clicked", "true");
+    
+    });
+
+}
+
+*/
